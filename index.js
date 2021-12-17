e12f8a31b150e67931fbc2fd9bb76a1a24ed0247
kuldeepsingh1828 <86602135+kuldeepSinghTrixAim@users.noreply.github.com>
2021-12-17 22:57:34 +0530

    changes save after morgan

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('first Node App')
+const express = require('express')
+const morgan = require('morgan')
+morgan();
+const server = express();
+server.listen(3000, () => {
+    console.log('server is running');
+});
